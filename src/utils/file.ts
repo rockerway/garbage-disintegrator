bf253a7ab9c5dd1bf581fe5f88f64e4e8b9e0377
rockerway <rockerway2@gmail.com>
2019-04-15 14:46:43 +0800

    fix: read file content error

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,18 +1,9 @@
 import fs = require('fs');
 
 export function readFile(path: string): string {
-  return fs
-    .readFileSync(path)
-    .toString()
-    .split('\n')
-    .slice(0, -1)
-    .join('\n');
+  return fs.readFileSync(path).toString();
 }
 
 export function writeFile(path: string, text: string): void {
   fs.writeFileSync(path, text);
-}
-
-export function appendTextToFile(path: string, text: string): void {
-  fs.appendFileSync(path, text);
 }
