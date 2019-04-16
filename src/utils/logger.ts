8b5b2487db4a046cd078a19ca91092d6d6ca873a
rockerway <rockerway2@gmail.com>
2019-04-16 13:02:27 +0800

    feature: define logger method argument type

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,14 +1,14 @@
 import chalk from 'chalk';
 
-export function info(message, type): void {
+export function info(message: string, type: string | void): void {
   const header = type ? `${chalk.green.bold(type)} ` : '';
   console.log(`${header}${message}`);
 }
 
-export function error(message): void {
+export function error(message: string): void {
   console.error(chalk.red(message));
 }
 
-export function success(message): void {
+export function success(message: string): void {
   console.error(chalk.green(message));
 }
