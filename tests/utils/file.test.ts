c46bb1e95b00949ed2cda1f02807a33049afcf0d
rockerway <rockerway2@gmail.com>
2019-04-16 13:00:40 +0800

    chore: renew file.test.ts to test read non-exist file and write already exist file

--- tests/utils/file.test.ts
+++ tests/utils/file.test.ts
@@ -1,9 +1,28 @@
-import { readFile } from '../../src/utils/file';
+import { readFile, writeFile } from '../../src/utils/file';
 
 describe('node js file system', () => {
   test('read file to string', () => {
     const fileContent = readFile(`${process.cwd()}/tests/data/file`);
+    const expected = 'test1\ntest2\ntest3';
 
-    expect(fileContent).toBe('test1\ntest2\ntest3');
+    expect(fileContent).toBe(expected);
+  });
+
+  test('read non-exist file', () => {
+    const filePath = `${process.cwd()}/tests/data/nonExistFile`;
+    const expected = `file: '${filePath}' does not exist.`;
+
+    expect(() => {
+      readFile(filePath);
+    }).toThrowError(expected);
+  });
+
+  test('write already exist file', () => {
+    const filePath = `${process.cwd()}/tests/data/existFile`;
+    const expected = `file: '${filePath}' already exist.`;
+
+    expect(() => {
+      writeFile(filePath, 'test');
+    }).toThrowError(expected);
   });
 });
